665baae045aacec8c9a0d00a2567bbb7777a4c7a
John Malvin <john.w.malvin@gmail.com>
2026-06-12 11:47:28 -0500

    added some extra quests and description mods for enchantments and effects

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -3,4 +3,12 @@ ItemEvents.tooltip(event => {
     event.addAdvanced(['minecraft:lantern', 'minecraft:soul_lantern'], (item, advanced, text) => {
         text.add(1, "Crouch+Right-Click to equip on your belt!");
     })
+
+    event.addAdvanced("vc_gliders:nether_upgrade", (item, advanced, text) => {
+        text.add(1, "Combine with a Glider in an Anvil with to protect it from the Nether heat!");
+    })
+
+    event.addAdvanced("vc_gliders:copper_upgrade", (item, advanced, text) => {
+        text.add(1, "Combine with a Glider in an Anvil with to protect it from lightning!");
+    })
 })
